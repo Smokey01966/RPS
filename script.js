ab5bc6edff3375d8f17d3f13855c2cf046e459aa
Smokey01966 <82545255+Smokey01966@users.noreply.github.com>
2023-05-29 18:54:47 -0400

    Build function to generate a random number

--- script.js
+++ script.js
@@ -3,8 +3,10 @@
 /* /Global Variables */
  
 
-/* Function randomNumber expects max and min (default 0) returns a random number between them */
-/* PCODE Generate random number with math then (number *(max - min +1)) + min*/
+/* Function randomNumber expects max and min (default 0) returns a random number between them inclusive */
+function calcRandomNumber (max, min = 0){
+    return Math.floor(Math.random() * (max - min +1)) + min;
+}
 
 /* Function getRandomChoice expects no input randomly choses between "rock" "paper" or "scissor"  */
 /* PCODE use randomNumber to get a number between 1 and 3 uses that number to choose "rock", "paper", or "scissors"
@@ -20,7 +22,7 @@
         else turn it into lower case
         if p1Choice is not rock, paper, or scissors, error "player 1 must choose rock paper or scissors." return error
     
-    Possibly generate, sanitize, and error check p2Choice
+    Possibly generate, sanitize, and error check p2Choiceg
         if p2Choice is empty set it to getRandomChoice
         else turn it into lower case
         if p2Choice is not rock, paper, or scissors, error "player 2 must choose rock paper or scissors." return error
